e2cd3182018338502649ead0e9a74b676f04b377
Akshay7772208 <134836865+Akshay7772208@users.noreply.github.com>
2024-01-19 18:48:02 -0800

    Update StarProduct.js

--- src/components/StarProduct.js
+++ src/components/StarProduct.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import '../styles/starProduct.css'
 
+
 const StarProduct = ({starProduct}) => {
+  const nothing="Image";
   return (
     <div className='starProduct'>
         <div>
             <a href={starProduct[0].url}><img src={starProduct[0].image}/></a>
         </div>
         <div>
-            <a href={starProduct[1].url}><img src={starProduct[1].image} alt={`image'}/></a>
-            <a href={starProduct[2].url}><img src={starProduct[2].image} alt={`image'}/></a>
-            <a href={starProduct[3].url}><img src={starProduct[3].image} alt={`image'}/></a>
+            <a href={starProduct[1].url}><img src={starProduct[1].image} alt={nothing}/></a>
+            <a href={starProduct[2].url}><img src={starProduct[2].image} alt={nothing}/></a>
+            <a href={starProduct[3].url}><img src={starProduct[3].image} alt={nothing}/></a>
             
         </div>
     </div>
