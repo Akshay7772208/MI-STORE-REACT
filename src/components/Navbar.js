e6f6085378756770941c9ccf889937a2eec037a5
Akshay7772208 <134836865+Akshay7772208@users.noreply.github.com>
2024-01-19 19:36:44 -0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,18 +10,18 @@ const Navbar=()=>{
         <div className="nav">
             <div className="logo">
                
-               <a href="/">
+               <a href="/#">
                     <img id="logoImage" src={data.logo} alt="Not" /> 
                </a>
             </div>
-             <a  className="navlinks" href="/miphones">Mi Phones</a>
-             <a   className="navlinks" href="/redmiphones">Redmi Phones</a>
-             <a   className="navlinks" href="/tv">TV</a>
-             <a   className="navlinks" href="/laptops">Laptops</a>
-             <a   className="navlinks" href="/lifestyle">Fitness & Lifestyle</a>
-             <a  className="navlinks" href="/home">Home</a>
-             <a   className="navlinks" href="/audio">Audio</a>
-             <a    className="navlinks" href="/accessories">Accessories</a>
+             <a  className="navlinks" href="/#miphones">Mi Phones</a>
+             <a   className="navlinks" href="/#redmiphones">Redmi Phones</a>
+             <a   className="navlinks" href="/#tv">TV</a>
+             <a   className="navlinks" href="/#laptops">Laptops</a>
+             <a   className="navlinks" href="/#lifestyle">Fitness & Lifestyle</a>
+             <a  className="navlinks" href="/#home">Home</a>
+             <a   className="navlinks" href="/#audio">Audio</a>
+             <a    className="navlinks" href="/#accessories">Accessories</a>
   
              <div className="searchbox">
                 <input type="text" name="search"  placeholder="Search Products"/>
